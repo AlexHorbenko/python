8c71b3634f503e18e5977863ccf92c8a0577b3c2
Alex <113252014+AlexHorbenko@users.noreply.github.com>
2024-09-04 13:29:08 +0300

    Add files via upload
    DZ_2.2

---
+++ dz_2.2.py
@@ -0,0 +1,16 @@
+import math
+
+user_number = float(input("Введіть п'ятизначне число:"))
+number = int(user_number)
+x = 10000
+y = 1000
+z = 100
+i = 10
+left_1, right = divmod(number, x)
+left_2, right_1 = divmod(right, y)
+left_3, right_2 = divmod(right_1, z)
+left_4, right_3 = divmod(right_2, i)
+#print(user_number)
+#print(number)
+#print(left_1, left_2, left_3, left_4, right_3)
+print(right_3, left_4, left_3, left_2, left_1)
