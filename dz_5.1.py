42fec328a5be6bf67d4a4a0b36d136cfd0f9fb40
Alex <113252014+AlexHorbenko@users.noreply.github.com>
2024-09-15 21:45:38 +0300

    Add files via upload
    DZ_5.1

--- dz_5.1.py
+++ dz_5.1.py
@@ -1,45 +1,58 @@
 import keyword
-
-from Tools.scripts.verify_ensurepip_wheels import print_notice
+import string
 
 user_text = input('Введіть текст:')
-print(user_text)
+#print(user_text)
 
-a = ' ' in user_text
-b = user_text.startswith('_')
-c = user_text.islower()
-d = user_text not in keyword.kwlist
-e = user_text.count("_") <= 1
-i = user_text[0].isdigit()
-k = '_' in user_text
-l = user_text[0].islower()
-m = '!' in user_text
+is_first_digit = user_text[0].isdigit()
+is_lower = user_text.islower()
+is_keyword = user_text in keyword.kwlist
+has_single_underscore = '_' in user_text
+has_double_underscore = user_text.count("__") > 0
+has_forbidden_sign = False
 
-if b and e:
-    print('True')
-else:
-    if c and k and i:
-        print('True1')
-    else:
-        if l and a != 1 and c and d and m != 1:
-            print('True2')
-        else:
-            print('False')
+has_forbidden_sign = any(char in string.punctuation.replace('_', '') or char == ' ' or char.isupper() for char in user_text)
+has_double_underscore = '__' in user_text
+
+result = True
+
+if is_keyword:
+    result = False
+
+if is_first_digit:
+    result = False
+
+if not is_lower and user_text.count('_') > 1:
+    result = False
+
+if has_double_underscore and user_text != '_':
+    result = False
+
+if has_forbidden_sign:
+    result = False
+
+if user_text == '_':
+    result =  True
+
+print(result)
 
 
 #_ #=> True
 #__ #=> False
 #___ #=> False
-#x #=> True
 #get_value #=> True
 #some_super_puper_value #=> True
 #assert_exception #=> True
-#m3 #=> True
 #3m #=> False
+
 #get value #=> False
-#Get_value #=> False
 #get!value #=> False
-#get_Value #=> False
-#getValue #=> False
 #assert #=> False
+
+#x #=> True
+#m3 #=> True
+
+#getValue #=> False
+#Get_value #=> False
+#get_Value #=> False
 
