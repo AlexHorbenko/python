c3dc601d68a7f565081c697f36d2df1ccb55e9b5
Alex <agorbenko1501@gmail.com>
2024-10-02 18:35:48 +0300

    dz10.2

--- test/test_1.py
+++ test/test_1.py
@@ -1,24 +1,23 @@
-from itertools import count
+import re
 
 
-def pow(x):
-    return x ** 2
+def first_word(text):
+    # Замінюємо крапки і коми на пробіли
+    text_edit = text.replace(".", " ").replace(",", " ")
+    # Розбиваємо текст на слова, автоматично прибираючи зайві пробіли
+    words = text_edit.split()
+    print(words)
+    # Шукаємо перше слово, що починається з літери
+    for word in words:
+        if re.match(r'^[a-zA-Z]', word):
+            return word
 
-from inspect import isgenerator
-def some_gen(begin, end, func):
-    list_gen = []
-    count = 0
-    while count < end:
-        yield begin
-        begin = func(begin)
-        list_gen.append(begin)
-        count += 1
 
-gen = some_gen(2, 4, pow)
-
-#print(isgenerator(gen))
-#print(list(gen))
-
-assert isgenerator(gen) == True, 'Test1'
-assert list(gen) == [2, 4, 16, 256], 'Test2'
+# Тести
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
 print('OK')
---
+++ test/test_2.py
@@ -0,0 +1,19 @@
+import re
+
+def first_word(text):
+    text_edit = text.replace(".", " ").replace(",", " ")
+    words = text_edit.split()
+    for word in words:
+        if re.match(r'^[a-zA-Z]', word):
+            #print(word)
+            return word
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
+
