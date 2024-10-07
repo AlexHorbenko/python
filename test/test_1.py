32947e74d0eb2f4aa0d7475fcacaff11433f4170
Alex <agorbenko1501@gmail.com>
2024-10-07 13:48:26 +0300

    dz 11.3

--- test/test_1.py
+++ test/test_1.py
@@ -1,15 +1,10 @@
-def generate_cube_numbers(end):
-    list_gen = []
-    for i in range(1, end+1):
-        if int(i ** 3) <= end:
-            list_gen += [i ** 3]
-            yield i ** 3
+def is_even(number):
+    if number & 1 == 0:
+        return True
+    else:
+        return False
 
-from inspect import isgenerator
 
-gen = generate_cube_numbers(1)
-
-assert isgenerator(gen) == True, 'Test0'
-assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
-assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
-assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
